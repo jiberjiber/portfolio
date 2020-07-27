e58faa3b45f54bc9fe26ccd518ef531a71a39545
Evan Boswood <evan@boswood.net>
2020-07-26 21:34:34 -0500

    initial commit

---
+++ js/pageControl.js
@@ -0,0 +1,56 @@
+// page control functions
+
+
+function navigate(tab){
+    pageContainer.children(`div#${tab}`).show();
+
+    for(let i = 0;i < pageContainer.children().length; i++){
+        if($(pageContainer.children()[i]).attr('id') === tab){
+            $(pageContainer.children()[i]).show('slow');
+        } else {
+            $(pageContainer.children()[i]).hide();
+        }
+    }
+}
+
+
+// page variables
+
+
+const pageContainer = $('#pageContainer');
+const home = $('#home');
+const webDev = $('#webDev');
+const production = $('#production');
+const contact = $('#contact');
+
+const homeBtnParent = $('#homeBtn');
+const webDevBtnParent = $('#webBtn');
+const productionBtnParent = $('#productionBtn');
+const contactBtnParent = $('#contactBtn');
+
+
+// button functions
+
+
+$('.navBtn').on('click',function(){
+    navigate($(this).attr('data-tab'));
+});
+
+
+// page load functions
+
+
+$(document).ready(function(){
+
+    $('.js-fullheight').css('height', $(window).height() - 20);
+    $(window).resize(function(){
+        $('.js-fullheight').css('height', $(window).height() - 20);
+    });
+
+    home.show();
+    homeBtnParent.addClass('uk-active');
+    webDev.hide();
+    production.hide();
+    contact.hide();
+
+  });
